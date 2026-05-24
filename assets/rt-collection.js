00d6ba848e05238d25e7fe28c798bf742064b5bf
Yashvardhan Singh Deora <yashvardhan1110@gmail.com>
2026-05-24 13:52:14 +0530

    auto: save changes [skip ci]

--- assets/rt-collection.js
+++ assets/rt-collection.js
@@ -24,13 +24,16 @@
       document.querySelector('header');
     var headerH = header ? header.offsetHeight : 61;
 
+    var pills = document.querySelector('.rt-pills');
+    var pillsH = pills ? pills.offsetHeight : 0;
+
     var toolbar = document.querySelector('.rt-toolbar');
     var toolbarH = toolbar ? toolbar.offsetHeight : 57;
 
     var root = document.documentElement;
     root.style.setProperty('--rt-header-h', headerH + 'px');
-    root.style.setProperty('--rt-toolbar-h', toolbarH + 'px');
-    root.style.setProperty('--rt-toolbar-bottom', (headerH + toolbarH) + 'px');
+    root.style.setProperty('--rt-pills-h', pillsH + 'px');
+    root.style.setProperty('--rt-toolbar-bottom', (headerH + pillsH + toolbarH) + 'px');
   }
 
   document.addEventListener('DOMContentLoaded', setHeightVars);
